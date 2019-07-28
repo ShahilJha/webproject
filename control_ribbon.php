e5f277c774d53e23ae5732b5b43fa1c212ceb40b
ShahilJha <shahil1.jha@gmail.com>
2019-07-28 20:29:24 +0545

    added 'visit site' option to control_ribbon.php

--- control_ribbon.php
+++ control_ribbon.php
@@ -2,4 +2,5 @@
 	<div class="control_option" style="margin-right: 2%;"><a href="backend/logout_process.php">Log-out</a></div>
 	<div class="control_option"><a href="signup.php">Add Auther</a></div>
 	<div class="control_option"><a href="admin_bash.php">Admin Bash</a></div>
+	<div class="control_option"><a href="index.php">Visit Site</a></div>
 </div>
